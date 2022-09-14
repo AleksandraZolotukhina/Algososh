82447114dcd0eb172852edcd6c519aed906f9f09
AlexandraZolotykhina <sashazolotykhina@mail.ru>
2022-09-14 18:27:09 +0300

    feat: Добавить анимацию в связный список

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
@@ -7,159 +7,147 @@ import structure from "../data-structure.module.css";
 import circles from "../wrapper-circles.module.css";
 import styles from "./list-page.module.css";
 import { ArrowIcon } from "../ui/icons/arrow-icon";
-class LinkedListNode {
-  value: any;
-  next: any;
-  constructor(value: any, next?: any) {
-    this.value = value;
-    this.next = next === undefined ? null : next;
-  }
-}
+import { setTimer } from "../../functions";
+import { ElementStates } from "../../types/element-states";
+import { LinkedList } from "./class-linked-list";
 
-class LinkedList {
-  private head: any;
-  private tail: any;
-  private size: any;
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-  prepend(value: any) {
-    const node = new LinkedListNode(value);
-    if (this.size === 0) {
-      this.tail = node;
-    }
-    else {
-      let cur = this.head;
-      while (cur.next) {
-        cur = cur.next
-      }
-      this.tail = cur;
-    }
-    node.next = this.head;
-    this.head = node;
-
-    this.size++;
-  }
-  append(value: any) {
-    const node = new LinkedListNode(value);
-    if (this.size === 0) {
-      this.head = node;
-      this.tail = node;
-    }
-    else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-    this.size++;
-  }
-  deleteHead() {
-    this.head = this.head.next;
-    this.size--;
-  }
-  deleteTail() {
-    if (this.size === 1) {
-      this.head = null;
-      this.tail = null;
-      this.size--;
-      return
-    }
-    let cur = this.head;
-    let index = 0;
-    while (index < this.size - 2) {
-      cur = cur.next;
-      index++;
-    }
-    cur.next = null;
-    this.tail = cur;
-    this.size--;
-  }
-
-  addByIndex(value: string, index: number) {
-    const node = new LinkedListNode(value);
-    if (index === 0) {
-      node.next = this.head;
-      this.head = node;
-    } else {
-      let cur = this.head;
-      let curIndex = 0;
-      while (curIndex < index - 1) {
-        cur = cur.next;
-        curIndex++;
-      }
-      node.next = cur.next;
-      cur.next = node;
-    }
-    this.size++;
-  }
-
-  deleteByIndex(index: number) {
-    if (index === 0) {
-      this.head = this.head.next;
-    }
-    else {
-      let cur = this.head;
-      let curIndex = 0;
-      while (curIndex < index - 1) {
-        cur = cur.next;
-        curIndex++;
-      }
-      cur.next = cur.next.next;
-    }
-    this.size--;
-  }
-
-  toArray() {
-    let cur = this.head;
-    const array = [];
-    while (cur) {
-      array.push(cur.value);
-      cur = cur.next;
-    }
-    return array;
-  }
-
-  getSize() {
-    return this.size;
-  }
-}
 export const ListPage: React.FC = () => {
   const [input, setInput] = useState<string>("");
   const [index, setIndex] = useState<string>("");
-  const [list] = useState(new LinkedList());
-  const [array, setArray] = useState<string[]>(["0", "34", "8", "1"]);
-  const getSmallCircles = (value: string | undefined, index: number| undefined, size: number) => {
-    const array = Array(size).fill(undefined);
-    if(index!==undefined){
-      array[index] = value;
-    }
-    return array;
+  const [list] = useState(new LinkedList(["0", "34", "8", "1"]));
+  const [array, setArray] = useState<string[] | undefined[]>(["0", "34", "8", "1"]);
+  const [smallCircles, setSmallCircles] = useState<[{ value: string, index: number }]>([{ value: "0", index: -1 }]);
+  const [indexPinkCircle, setIndexPinkCircle] = useState<number>(-1);
+  const [indexGreenCircle, setIndexGreenCircle] = useState<number>(-1);
+  const [isBottomElement, setIsBottomElement] = useState<boolean>(false);
+
+  const addHead = () => {
+    setSmallCircles([{ value: input, index: 0 }]) //отрисовываем новый элемент
+    list.prepend(input);
   }
+
+  const addTail = () => {
+    setSmallCircles([{ value: input, index: list.getSize() - 1 }]) //отрисовываем новый элемент
+    list.append(input);
+  }
+
+  const addElement = async (add: any, index: number) => {
+    add();
+    setIsBottomElement(false);
+    await setTimer(500);
+
+    setSmallCircles([{ value: input, index: -1 }]) //удаляем отрисованный новый элемент
+    setArray([...list.toArray()]); //добавляем в массив новый элемент
+    setIndexGreenCircle(index); //выделяем зеленым, что добавили новый элемент
+    await setTimer(500);
+
+    setIndexGreenCircle(-1); //убираем выделение цвета, что добавили новый элемент
+  }
+  const deleteHead = () => {
+    list.deleteHead()
+  }
+
+  const deleteTail = () => {
+    list.deleteTail()
+  }
+
+  const deleteElement = async (func: any, index: number) => {
+    setSmallCircles([{ value: array[index] as string, index: index }])
+    array.splice(index, 1, undefined);
+    setArray([...array] as any);
+    func(); // удаляем элемент
+    await setTimer(500);
+
+    array.splice(index, 1);
+    setArray([...array] as any);
+    setSmallCircles([{ value: input, index: -1 }]) // убираем новый отрисованный элемент
+    setIsBottomElement(false)
+  }
+
+  const deleteByIndex = async (index: number) => {
+    setIsBottomElement(true);
+    let curIndex = 0;
+    while (curIndex <= Number(index) + 1) {
+      setIndexPinkCircle(curIndex);
+      await setTimer(500);
+      curIndex++;
+    }
+    setSmallCircles([{ value: array[index] as string, index: index }])
+
+
+    array.splice(index, 1, undefined);
+    setArray([...array] as any);
+    await setTimer(500);
+
+    setIndexPinkCircle(-1);
+    setSmallCircles([{ value: input, index: -1 }]) // убираем новый элемент
+    list.deleteByIndex(index);
+    setArray([...list.toArray()])
+  }
+
+  const addByIndex = async () => {
+    list.addByIndex(input, Number(index));
+    let curIndex = 0;
+    let state;
+    while (curIndex <= Number(index) + 1) {
+      state = smallCircles.splice(0, 1, { value: input, index: curIndex });
+      setSmallCircles([...state] as any); //отрисовываем новый элемент
+      await setTimer(500);
+
+      setIndexPinkCircle(curIndex);
+      curIndex++;
+    }
+
+    setIndexPinkCircle(-1); // убираем розовый цвет
+    setSmallCircles([{ value: input, index: -1 }]) // убираем новый элемент
+    setArray([...list.toArray()]) // добавляем новый эелемент в массив
+    setIndexGreenCircle(Number(index)); //выделяем зеленым, что добавили новый элемент
+    await setTimer(500);
+    setIndexGreenCircle(-1); //убираем выделение цвета, что добавили новый элемент
+  }
+
+  const getColorCircle = (index: number) => {
+    if (index < indexPinkCircle) {
+      return ElementStates.Changing;
+    }
+    if (index === indexGreenCircle) {
+      return ElementStates.Modified;
+    }
+    return ElementStates.Default;
+  }
+
   return (
     <SolutionLayout title="Связный список">
       <div className={`${structure.actions} ${styles.actions}`}>
         <Input isLimitText={true} type={"number"} max={19} value={input} onChange={(e) => setInput((e.target as HTMLInputElement).value)} />
-        <Button text={"Добавить в head"} onClick={() => { list.prepend(input); setArray([...list.toArray()]); getSmallCircles(input, 0, list.getSize()) } } />
-        <Button text={"Добавить в tail"} onClick={() => { list.append(input); setArray([...list.toArray()]) }} />
-        <Button text={"Удалить из head"} onClick={() => { list.deleteHead(); setArray([...list.toArray()]) }} />
-        <Button text={"Удалить из tail"} onClick={() => { list.deleteTail(); setArray([...list.toArray()]) }} />
+        <Button text={"Добавить в head"} onClick={() => addElement(addHead, 0)} />
+        <Button text={"Добавить в tail"} onClick={() => addElement(addTail, list.getSize())} />
+        <Button text={"Удалить из head"} onClick={() => { setIsBottomElement(true); deleteElement(deleteHead, 0) }} />
+        <Button text={"Удалить из tail"} onClick={() => { setIsBottomElement(true); deleteElement(deleteTail, list.getSize() - 1) }} />
       </div>
       <div className={`${structure.actions} ${styles.actions}`}>
         <Input isLimitText={true} type={"number"} max={19} value={index} onChange={(e) => setIndex((e.target as HTMLInputElement).value)} />
-        <Button text={"Добавить по индексу"} onClick={() => { list.addByIndex(input, Number(index)); setArray([...list.toArray()]) }} />
-        <Button text={"Удалить по индексу"} onClick={() => { list.deleteByIndex(Number(index)); setArray([...list.toArray()]) }} />
+        <Button text={"Добавить по индексу"} onClick={() => addByIndex()} />
+        <Button text={"Удалить по индексу"} onClick={() => deleteByIndex(Number(index))} />
       </div>
-      <div className={circles.wrapper}>
+      <div className={circles.wrapper} style={{ justifyContent: "flex-start", position: "relative" }}>
         {
-          getSmallCircles(undefined,undefined,array.length).map((el:string,index:number)=>{
-            <Circle letter={el} index={index} />
+
+          smallCircles.map(el => {
+            let marginLeft = `calc(((100% - 136px*${array.length - 1} - 80px)/2) + 14px + ${el.index}*136px)`;
+            return el.index !== -1 ?
+              <div key={el.index} style={{ zIndex: "2", position: "absolute", marginTop: 0, marginLeft: marginLeft, top: isBottomElement ? '162px' : '-16px' }}>
+                <Circle letter={el.value} isSmall={true} state={ElementStates.Changing} />
+              </div> :
+              <></>
           })
         }
-        {array.map((el: string, index: number) => {
+      </div>
+      <div className={circles.wrapper}>
+        {array.map((el: string | undefined, index: number) => {
           return (
             <Fragment key={index}>
-              <Circle letter={el} index={index} head={index === 0 ? "head" : ""} tail={index === array.length - 1 ? "tail" : ""} />
+              <Circle letter={el} state={getColorCircle(index)} index={index} head={index === 0 ? "head" : ""} tail={index === array.length - 1 ? "tail" : ""} />
               {index < array.length - 1 ? <ArrowIcon /> : <></>}
             </Fragment>
           )
